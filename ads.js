64f3213159bdc432acdec28262ce0bc2a0825db3
Hossein <153190272+TheodoreBagwell1@users.noreply.github.com>
2024-05-14 08:56:27 -0700

    Update ads.js

--- ads.js
+++ ads.js
@@ -76,73 +76,83 @@ var cssStyles = `
   }
 }
 `;
-
-// Create a style element and append CSS styles
-var style = document.createElement("style");
-style.textContent = cssStyles;
-document.head.appendChild(style);
-
-// Dynamically create modal element and its child elements and append them to the body
-var modal = document.createElement("div");
-modal.id = "myModal";
-modal.classList.add("modal");
-document.body.appendChild(modal);
-
-var closeButton = document.createElement("span");
-closeButton.classList.add("close");
-closeButton.innerHTML = "&times;";
-modal.appendChild(closeButton);
-
-var modalLink = document.createElement("a");
-modalLink.href = "https://t.me/codecanyon_bylancer_bot";
-modal.appendChild(modalLink);
-
-var modalImg = document.createElement("img");
-modalImg.classList.add("modal-content");
-modalImg.id = "img01";
-modalImg.src =
-  "https://raw.githubusercontent.com/TheodoreBagwell1/shell-media/main/poster.webp";
-modalLink.appendChild(modalImg);
-
-var caption = document.createElement("div");
-caption.id = "caption";
-modal.appendChild(caption);
-
-// Dynamically create image element and append it to the body
-var img = document.createElement("img");
-img.id = "myImg";
-img.src =
-  "https://github.com/TheodoreBagwell1/shell-media/blob/main/gift.gif?raw=true";
-img.alt = "Snow";
-img.style.width = "100%";
-img.style.maxWidth = "200px";
-img.style.borderRadius = "5px";
-img.style.cursor = "pointer";
-img.style.transition = "0.3s";
-img.style.position = "fixed";
-img.style.bottom = "20px";
-img.style.right = "20px";
-img.addEventListener("click", function () {
-  modal.style.display = "block";
-});
-document.body.appendChild(img);
-
-// Set up event listener for modal close button
-closeButton.addEventListener("click", function () {
-  modal.style.display = "none";
-});
-
-
-// Check if the modal has been opened before
-if (localStorage.getItem("modalOpened") === "true") {
-  modal.style.display = "none"; // Hide modal if already opened twice
-} else {
-  // Display modal if not opened before
-  modal.style.display = "block";
-  localStorage.setItem("modalOpened", "true");
+function isCodecanyanUrl(url) {
+  return url.includes("codecanyon");
 }
 
-setTimeout(() => {
-  localStorage.setItem("modalOpened", "false");
+// Example usage:
+const url1 = window.location.href;
 
-}, 60000 * 4);
+if (isCodecanyanUrl(url1)) {
+  ads();
+}
+
+function ads(params) {
+  // Create a style element and append CSS styles
+  var style = document.createElement("style");
+  style.textContent = cssStyles;
+  document.head.appendChild(style);
+
+  // Dynamically create modal element and its child elements and append them to the body
+  var modal = document.createElement("div");
+  modal.id = "myModal";
+  modal.classList.add("modal");
+  document.body.appendChild(modal);
+
+  var closeButton = document.createElement("span");
+  closeButton.classList.add("close");
+  closeButton.innerHTML = "&times;";
+  modal.appendChild(closeButton);
+
+  var modalLink = document.createElement("a");
+  modalLink.href = "https://t.me/codecanyon_bylancer_bot";
+  modal.appendChild(modalLink);
+
+  var modalImg = document.createElement("img");
+  modalImg.classList.add("modal-content");
+  modalImg.id = "img01";
+  modalImg.src =
+    "https://raw.githubusercontent.com/TheodoreBagwell1/shell-media/main/poster.webp";
+  modalLink.appendChild(modalImg);
+
+  var caption = document.createElement("div");
+  caption.id = "caption";
+  modal.appendChild(caption);
+
+  // Dynamically create image element and append it to the body
+  var img = document.createElement("img");
+  img.id = "myImg";
+  img.src =
+    "https://github.com/TheodoreBagwell1/shell-media/blob/main/gift.gif?raw=true";
+  img.alt = "Snow";
+  img.style.width = "100%";
+  img.style.maxWidth = "200px";
+  img.style.borderRadius = "5px";
+  img.style.cursor = "pointer";
+  img.style.transition = "0.3s";
+  img.style.position = "fixed";
+  img.style.bottom = "20px";
+  img.style.right = "20px";
+  img.addEventListener("click", function () {
+    modal.style.display = "block";
+  });
+  document.body.appendChild(img);
+
+  // Set up event listener for modal close button
+  closeButton.addEventListener("click", function () {
+    modal.style.display = "none";
+  });
+
+  // Check if the modal has been opened before
+  if (localStorage.getItem("modalOpened") === "true") {
+    modal.style.display = "none"; // Hide modal if already opened twice
+  } else {
+    // Display modal if not opened before
+    modal.style.display = "block";
+    localStorage.setItem("modalOpened", "true");
+  }
+
+  setTimeout(() => {
+    localStorage.setItem("modalOpened", "false");
+  }, 60000 * 4);
+}
